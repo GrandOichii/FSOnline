6cbde1b01376abbaf5324d8372238cd11b145777
Igor Yastrebov <i.hawk@yandex.ru>
2024-07-18 06:35:19 -0400

    Cards: add Portable Slot Machine

---
+++ cards/testing/treasures/portable-slot-machine-v2.lua
@@ -0,0 +1,24 @@
+
+function _Create()
+    return FS.B.Item()
+        :ActivatedAbility(
+            FS.B.ActivatedAbility('Pay 3{cent}', 'Roll-\n1-2: Loot 1.\n3-4: Gain 4{cent}.')
+                .Cost:Common(
+                    FS.C.Cost.PayCoins(3)
+                )
+                .Effect:Roll(function (stackEffect)
+                    local roll = stackEffect.Rolls[0]
+
+                    if roll == 1 or roll == 2 then
+                        FS.C.Effect.Loot(1)(stackEffect)
+                        return
+                    end
+                    if roll == 3 or roll == 4 then
+                        FS.C.Effect.GainCoins(4)(stackEffect)
+                        return
+                    end
+                end)
+            :Build()
+        )
+    :Build()
+end
