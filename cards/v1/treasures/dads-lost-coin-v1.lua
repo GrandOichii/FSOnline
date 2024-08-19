5e9011005d3a6bc94e70415efa142c05d6576ac9
Igor Yastrebov <i.hawk@yandex.ru>
2024-08-19 17:31:08 +0300

    Cards: add v1 Dad's Lost Coin

---
+++ cards/v1/treasures/dads-lost-coin-v1.lua
@@ -0,0 +1,28 @@
+-- status: implemented
+-- TODO this works differently that v2
+
+function _Create()
+    return FS.B.Item()
+        -- When anyone rolls a {roll:1}, you may force that player to reroll it.
+        .Static:Raw(
+            FS.ModLayers.ROLL_REPLACEMENT_EFFECTS,
+            function (me)
+                local players = GetPlayers()
+                for _, player in ipairs(players) do
+                    player.State.RollReplacementEffects:Add(function (rollStackEffect)
+                        local roll = rollStackEffect.Value
+                        if roll ~= 1 then
+                            return true
+                        end
+                        local accept = FS.C.Choose.YesNo(me.Owner.Idx, 'Player '..player.Name..' rolled a '..roll..'. Force them to reroll?')
+                        if not accept then
+                            return true
+                        end
+                        RerollDice(rollStackEffect)
+                        return false
+                    end)
+                end
+            end
+        )
+    :Build()
+end
