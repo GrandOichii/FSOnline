0d76b0cbdf777291da46da6239a087775b5af99d
Igor Yastrebov <i.hawk@yandex.ru>
2024-08-22 15:04:18 +0300

    Cards: add Smelter

---
+++ cards/testing/treasures/smelter-v2.lua
@@ -0,0 +1,16 @@
+-- status: not tested
+
+function _Create()
+    return FS.B.Item()
+        :ActivatedAbility(
+            FS.B.ActivatedAbility('Discard a loot card', 'Gain 3{cent}')
+                .Cost:Common(
+                    FS.C.Cost.DiscardLoot(1)
+                )
+                .Effect:Common(
+                    FS.C.Effect.GainCoins(3)
+                )
+            :Build()
+        )
+    :Build()
+end
