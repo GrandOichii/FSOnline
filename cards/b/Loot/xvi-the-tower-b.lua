fa17978c0953361ee5ba1da9f32d556390513247
Igor Yastrebov <i.hawk@yandex.ru>
2024-09-05 11:28:56 +0300

    Cards: add base game XVI. The Tower

---
+++ cards/b/Loot/xvi-the-tower-b.lua
@@ -0,0 +1,16 @@
+-- status: not tested
+
+function _Create()
+    return FS.B.Loot('Toll:\n1-2: All players take 1 damage.\n3-4: All monsterss take 1 damage.\n5-6: All players take 2 damage.')
+        .Effect:Roll(
+            FS.C.SwitchRoll(0, {
+                [1] = FS.C.Effect.DamageToPlayer(1, FS.C.AllPlayers),
+                [2] = FS.C.Effect.DamageToPlayer(1, FS.C.AllPlayers),
+                [3] = FS.C.Effect.DamageToMonster(1, FS.C.AllPlayers),
+                [4] = FS.C.Effect.DamageToMonster(1, FS.C.AllPlayers),
+                [5] = FS.C.Effect.DamageToPlayer(2, FS.C.AllPlayers),
+                [6] = FS.C.Effect.DamageToPlayer(2, FS.C.AllPlayers),
+            })
+        )
+    :Build()
+end
