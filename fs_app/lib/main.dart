5fa3aa1a9f6fe2c4f8e19d55d80b4ceca470da04
Igor Yastrebov <i.hawk@yandex.ru>
2024-09-09 22:47:51 +0300

    fs_app: Add all cards display

--- fs_app/lib/main.dart
+++ fs_app/lib/main.dart
@@ -4,6 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:fs_app/card.dart';
 import 'package:http/http.dart' as http;
 
+const host = 'localhost';
+// const host = '10.0.2.2';
+
 void main() {
   runApp(FSApp());
 }
@@ -44,13 +47,15 @@ class _CardSearchState extends State<CardSearch> {
 
   Widget getCardWidget() {
     return (fetchedCard == null)
-      ? const Text('No card found')
-      : FSCardView(card: fetchedCard!);
+        ? const Text('No card found')
+        : FSCardView(card: fetchedCard!);
   }
 
   void fetchCard() async {
     var cardKey = searchController.text;
-    var resp = await http.get(Uri.parse('http://10.0.2.2:5000/api/v1/Card/$cardKey'));
+
+    var resp =
+        await http.get(Uri.parse('http://$host:5000/api/v1/Card/$cardKey'));
     if (resp.statusCode != 200) {
       throw Exception('Failed to fetch card with key $cardKey');
     }
@@ -62,29 +67,97 @@ class _CardSearchState extends State<CardSearch> {
   }
 }
 
+class FSDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          ListTile(
+            title: const Text('All cards'),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.pushNamed(context, '/');
+            },
+          ),
+          ListTile(
+            title: const Text('By card key'),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.pushNamed(context, '/bykey');
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+Scaffold createScaffold(String title, Widget body) {
+  return Scaffold(
+    appBar: AppBar(
+      backgroundColor: Colors.orange,
+      title: Text(title),
+    ),
+    body: body,
+    drawer: FSDrawer(),
+  );
+}
+
+class KeySearchPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return createScaffold('Card Search', CardSearch());
+  }
+}
+
+class AllCardsPage extends StatefulWidget {
+  @override
+  State<AllCardsPage> createState() => _AllCardsPageState();
+}
+
+class _AllCardsPageState extends State<AllCardsPage> {
+  List<FSCard>? cards;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchCards();
+  }
+
+  void fetchCards() async {
+    var resp = await http.get(Uri.parse('http://$host:5000/api/v1/Card'));
+    var data = jsonDecode(resp.body) as List<dynamic>;
+    setState(() {
+      cards = data.map((e) => FSCard.fromJson(e)).toList();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return createScaffold(
+        'Home',
+        (cards == null)
+            ? const Text('Fetching cards...')
+            : ListView.builder(
+                scrollDirection: Axis.vertical,
+                itemCount: cards!.length,
+                itemBuilder: (ctx, index) {
+                  return FSCardView(card: cards![index]);
+                },
+              ));
+  }
+}
+
 class FSApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.orange,
-          title: const Text('FSApp'),
-        ),
-        body: CardSearch(),
-        drawer: Drawer(
-          child: ListView(
-            children: [
-              ListTile(
-                title: const Text('All cards'),
-                onTap: () {
-                  print('amogus');
-                },
-              )
-            ],
-          ),
-        ),
-      ),
+      routes: {
+        '/': (ctx) => AllCardsPage(),
+        '/bykey': (ctx) => KeySearchPage(),
+      },
+      // home: AllCardsPage(),
     );
   }
 }
