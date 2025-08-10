17c32cd72b1aef520f6585f86bff388c37faa7a6
Igor Yastrebov <i.hawk@yandex.ru>
2025-08-10 15:49:29 +0300

    FSCore.Tests: fully implemented tests for Pills (White-Blue)

--- FSCore.Tests/Matches/Loot/PillsWhiteBlue.cs
+++ FSCore.Tests/Matches/Loot/PillsWhiteBlue.cs
@@ -49,5 +49,48 @@ public class PillsWhiteBlueTests
         match.AssertCardsInLootDeck(lootDeckSize - 1 - lootAmount);
     }
 
-    // TODO add tests for rolls 5 and 6
+    [Theory]
+    [InlineData(5, 2)]
+    [InlineData(6, 2)]
+    public async Task DiscardRolls(int rollResult, int expectedHandSize)
+    {
+        // Arrange
+        var mainPlayerIdx = 0;
+        var cardKey = "pills-white-blue-b";
+        var lootDeckSize = 10;
+        var config = new MatchConfigBuilder()
+            .InitialCoins(0)
+            .InitialLoot(3)
+            .ConfigLootDeck().Add(cardKey, lootDeckSize).Done()
+            .Build();
+
+        var roller = new ProgrammedRollerBuilder()
+            .Then(rollResult)
+            .Build();
+
+        var mainPlayer = new ProgrammedPlayerControllerBuilder("isaac-b")
+            .ConfigActions()
+                .AssertIsCurrentPlayer()
+                .PlayLootCard(cardKey)
+                .Choose.CardInHand(0)
+                .AutoPassUntilEmptyStack()
+                .SetWinner()
+                .Done()
+            .Build();
+
+        List<ProgrammedPlayerController> players = [
+            mainPlayer,
+            ProgrammedPlayerControllers.AutoPassPlayerController("judas-b"),
+        ];
+
+        var match = new TestMatch(config, mainPlayerIdx, roller: roller);
+        await match.AddPlayers(players);
+
+        // Act
+        await match.Run();
+
+        // Assert
+        match.AssertIsWinner(mainPlayerIdx);
+        match.AssertCardsInHand(mainPlayerIdx, expectedHandSize);
+    }
 }
