b5bef1bc9bf43bc9e3f99fbec47195694f125e23
Igor Yastrebov <i.hawk@yandex.ru>
2025-09-07 17:39:27 +0300

    FSCore.Tests: add additional test for Meat and Synthoil

--- FSCore.Tests/Matches/Cards/Treasures/MeatSynthoil.cs
+++ FSCore.Tests/Matches/Cards/Treasures/MeatSynthoil.cs
@@ -49,4 +49,55 @@ public class MeatSynthoilTests
             .IsWinner()
             .HasSoulCard(monsterKey);
     }
+
+    [Theory]
+    [InlineData("meat-b")]
+    [InlineData("synthoil-b")]
+    public async Task DoesntRecharge(string cardKey)
+    {
+        // Arrange
+        var mainPlayerIdx = 0;
+        var activatable = "the-butter-bean-b2";
+        var config = new MatchConfigBuilder()
+            .InitialCoins(0)
+            .InitialLoot(0)
+            .LootStepLootAmount(0)
+            .LootPlay(0)
+            .InitialTreasureSlots(0)
+            .Build();
+
+        var mainPlayer = new ProgrammedPlayerControllerBuilder("isaac-b")
+            .HasItemAtStart(cardKey)
+            .HasItemAtStart(activatable)
+            .ConfigActions()
+                .AssertIsCurrentPlayer()
+                .ActivateOwnedItem(activatable, 0)
+                .AutoPassUntilEmptyStack()
+                .SetWinner()
+                .Done()
+            .Build();
+
+        var roller = new ProgrammedRollerBuilder()
+            .Then(5)
+            .Build();
+
+        List<ProgrammedPlayerController> players = [
+            mainPlayer,
+            ProgrammedPlayerControllers.AutoPassPlayerController("judas-b"),
+        ];
+
+        var match = new TestMatch(config, mainPlayerIdx, roller: roller);
+        await match.AddPlayers(players);
+
+        // Act
+        await match.Run();
+
+        // Assert
+        match.AssertPlayer(mainPlayerIdx)
+            .IsWinner();
+        match.AssertSingleItem(mainPlayerIdx, activatable)
+            .IsTapped();
+    }
+
+
 }
