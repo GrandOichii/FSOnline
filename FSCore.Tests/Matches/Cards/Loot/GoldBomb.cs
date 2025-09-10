ba8add050b76e8ff6dc9e71d81c4aef7d66435a2
Igor Yastrebov <i.hawk@yandex.ru>
2025-09-10 16:39:25 +0300

    FSCore.Tests: add tests for Gold Bomb

---
+++ FSCore.Tests/Matches/Cards/Loot/GoldBomb.cs
@@ -0,0 +1,140 @@
+namespace FSCore.Tests.Matches.Cards.Loot;
+
+public class GoldBombTests
+{
+    private static readonly string CARD_KEY = "gold-bomb-b";
+
+    [Fact]
+    public async Task OnSelf()
+    {
+        // Arrange
+        var mainPlayerIdx = 0;
+        var lootDeckSize = 10;
+        var config = new MatchConfigBuilder()
+            .InitialCoins(0)
+            .InitialLoot(0)
+            .InitialMonsterSlots(1)
+            .ConfigMonsterDeck().AddMonster("clotty-b").Done()
+            .ConfigLootDeck().Add(CARD_KEY, lootDeckSize).Done()
+            .Build();
+
+        var mainPlayer = new ProgrammedPlayerControllerBuilder("isaac-b")
+            .ConfigActions()
+                .AssertIsCurrentPlayer()
+                .PreventDamage(3)
+                .PlayLootCard(CARD_KEY)
+                    .Choose.Me()
+                .AutoPassUntilEmptyStack()
+                .SetWinner()
+                .Done()
+            .Build();
+
+        List<ProgrammedPlayerController> players = [
+            mainPlayer,
+            ProgrammedPlayerControllers.AutoPassPlayerController("judas-b"),
+        ];
+
+        var match = new TestMatch(config, mainPlayerIdx);
+        await match.AddPlayers(players);
+
+        // Act
+        await match.Run();
+
+        // Assert
+        match.AssertPlayer(mainPlayerIdx)
+            .IsWinner()
+            .HasNoDamagePreventors()
+            .HasNoDamage();
+    }
+
+    [Fact]
+    public async Task OnOpponent()
+    {
+        // Arrange
+        var mainPlayerIdx = 0;
+        var opponentIdx = 1 - mainPlayerIdx;
+        var lootDeckSize = 10;
+        var config = new MatchConfigBuilder()
+            .InitialCoins(0)
+            .InitialLoot(0)
+            .InitialMonsterSlots(1)
+            .ConfigMonsterDeck().AddMonster("clotty-b").Done()
+            .ConfigLootDeck().Add(CARD_KEY, lootDeckSize).Done()
+            .Build();
+
+        var mainPlayer = new ProgrammedPlayerControllerBuilder("isaac-b")
+            .ConfigActions()
+                .AssertIsCurrentPlayer()
+                .PlayLootCard(CARD_KEY)
+                    .Choose.Player(opponentIdx)
+                .AutoPassUntilEmptyStack()
+                .SetWinner()
+                .Done()
+            .Build();
+
+        List<ProgrammedPlayerController> players = [
+            mainPlayer,
+            new ProgrammedPlayerControllerBuilder("judas-b")
+                .ConfigActions()
+                    .PreventDamage(3)
+                    .AutoPass()
+                    .Done()
+                .Build(),
+        ];
+
+        var match = new TestMatch(config, mainPlayerIdx);
+        await match.AddPlayers(players);
+
+        // Act
+        await match.Run();
+
+        // Assert
+        match.AssertPlayer(mainPlayerIdx)
+            .IsWinner();
+        match.AssertPlayer(opponentIdx)
+            .HasNoDamagePreventors()
+            .HasNoDamage();
+    }
+
+    [Fact]
+    public async Task OnMonster()
+    {
+        // Arrange
+        var mainPlayerIdx = 0;
+        var lootDeckSize = 10;
+        var config = new MatchConfigBuilder()
+            .InitialCoins(0)
+            .InitialLoot(0)
+            .InitialMonsterSlots(1)
+            .ConfigMonsterDeck().AddMonster("monstro-b").Done()
+            .ConfigLootDeck().Add(CARD_KEY, lootDeckSize).Done()
+            .Build();
+
+        var mainPlayer = new ProgrammedPlayerControllerBuilder("isaac-b")
+            .ConfigActions()
+                .AssertIsCurrentPlayer()
+                .PlayLootCard(CARD_KEY)
+                    .Choose.MonsterInSlot(0)
+                .AutoPassUntilEmptyStack()
+                .SetWinner()
+                .Done()
+            .Build();
+
+        List<ProgrammedPlayerController> players = [
+            mainPlayer,
+            ProgrammedPlayerControllers.AutoPassPlayerController("judas-b"),
+        ];
+
+        var match = new TestMatch(config, mainPlayerIdx);
+        await match.AddPlayers(players);
+
+        // Act
+        await match.Run();
+
+        // Assert
+        match.AssertPlayer(mainPlayerIdx)
+            .IsWinner();
+        match.AssertMonsterInSlot(0)
+            .HasDamage(3);
+    }
+}
