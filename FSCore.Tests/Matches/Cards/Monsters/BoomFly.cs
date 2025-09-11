e8bc6e6a6b9437e6bba72bf52d8c202b18fd179e
Igor Yastrebov <i.hawk@yandex.ru>
2025-09-11 16:33:33 +0300

    FSCore.Tests: add test for Boom Fly

---
+++ FSCore.Tests/Matches/Cards/Monsters/BoomFly.cs
@@ -0,0 +1,54 @@
+namespace FSCore.Tests.Matches.Cards.Monsters;
+
+public class BoomFly
+{
+    [Fact]
+    public static async Task DeathTrigger()
+    {
+        // Arrange
+        var monsterCardKey = "boom-fly-b";
+        var mainPlayerIdx = 0;
+        var opponentIdx = 1 - mainPlayerIdx;
+        var config = new MatchConfigBuilder()
+            .InitialCoins(0)
+            .InitialLoot(0)
+            .LootStepLootAmount(0)
+            .LootPlay(0)
+            .InitialTreasureSlots(0)
+            .InitialMonsterSlots(1)
+            .ConfigMonsterDeck().AddMonster(monsterCardKey).Done()
+            .Build();
+
+        var mainPlayer = new ProgrammedPlayerControllerBuilder("isaac-b")
+            .ConfigActions()
+                .DeclareAttack(0)
+                .AutoPassUntilEmptyStack()
+                .SetWinner()
+                .Done()
+            .Build();
+
+        var roller = new ProgrammedRollerBuilder()
+            .Then(4)
+            .Build();
+
+        List<ProgrammedPlayerController> players = [
+            mainPlayer,
+            ProgrammedPlayerControllers.AutoPassPlayerController("judas-b"),
+        ];
+
+        var match = new TestMatch(config, mainPlayerIdx, roller: roller);
+        await match.AddPlayers(players);
+
+        // Act
+        await match.Run();
+
+        // Assert
+        match.AssertPlayer(mainPlayerIdx)
+            .IsWinner()
+            .HasDamage(1)
+            .HasCoins(4);
+        match.AssertPlayer(opponentIdx)
+            .HasDamage(1);
+    }
+
+}
